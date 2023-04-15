5d9bdce95ab37ff959ed9261107930b4bb4895c4
robi leksono <robileksono@gmail.com>
2023-04-15 19:08:29 +0800

    Symptom Feature
    add needed file for symptom feature

---
+++ app/Http/Controllers/Pakar/SymptomController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSymptomRequest;
+use App\Http\Requests\UpdateSymptomRequest;
+use App\Models\Symptom;
+
+class SymptomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSymptomRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Symptom $symptom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Symptom $symptom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSymptomRequest $request, Symptom $symptom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Symptom $symptom)
+    {
+        //
+    }
+}
