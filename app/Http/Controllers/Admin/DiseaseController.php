6b793e364fb6a1da0fe15f1ff545e64372f6e620
robi leksono <robileksono@gmail.com>
2023-04-15 19:21:52 +0800

    Disease Feature
    update folder placement and namespace

--- app/Http/Controllers/Admin/DiseaseController.php
+++ app/Http/Controllers/Admin/DiseaseController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\StoreDiseaseRequest;
-use App\Http\Requests\UpdateDiseaseRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Disease\StoreDiseaseRequest;
+use App\Http\Requests\Admin\Disease\UpdateDiseaseRequest;
 use App\Models\Disease;
 
 class DiseaseController extends Controller
--- app/Http/Requests/Admin/Disease/StoreDiseaseRequest.php
+++ app/Http/Requests/Admin/Disease/StoreDiseaseRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Admin\Disease;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateDiseaseRequest extends FormRequest
+class StoreDiseaseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
