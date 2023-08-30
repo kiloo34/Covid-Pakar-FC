ef61ebfa1ade71ecfd9a80228c1fcb228ee2703f
robi leksono <robileksono@gmail.com>
2023-08-30 22:27:15 +0800

    add disease category in pakar actor

--- resources/views/pakar/disease/category/create.blade.php
+++ resources/views/pakar/disease/category/create.blade.php
@@ -10,15 +10,27 @@
                 </div>
             </div>
             <div class="card-body">
-                <form action="{{ route('admin.penyakit.store') }}" method="post">
-                    {{-- {{ route('tipe.store') }} --}}
+                <form action="{{ route('pakar.kategori_penyakit.store') }}" method="post">
                     @csrf
                     <div class="form-group">
-                        {{-- <label>{{ __("Nama Wabah / Penyakit / Virus") }}</label> --}}
-                        <label>Nama Wabah / Penyakit / Virus</label>
-                        <input name="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                            value="{{ old('name') }}" autocomplete="name" autofocus>
-                        @error('name')
+                        <label>{{ __("Nama Wabah / Penyakit / Virus") }}</label>
+                        <select name="disease" class="form-control @error('disease') is-invalid @enderror" value="{{ old('disease') }}" autofocus>
+                            <option value="">{{ __("Pilih Nama Wabah / Penyakit / Virus") }}</option>
+                            @foreach ($diseases as $disease)
+                                <option value="{{$disease->id}}">{{ucfirst($disease->name)}}</option>
+                            @endforeach
+                        </select>
+                        @error('disease')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    
+                    <div class="form-group">
+                        <label>{{ __("Kategori Penyakit") }}</label>
+                        <input type="text" name="category" class="form-control @error('category') is-invalid @enderror" value="{{ old('category') }}" autocomplete="category" autofocus>
+                        @error('category')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
