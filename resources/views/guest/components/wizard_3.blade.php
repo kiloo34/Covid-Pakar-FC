70b700cd835269ccc232b3396b67fb2503e6f893
robi leksono <robileksono@gmail.com>
2023-11-14 15:58:24 +0800

    add dashboard view

--- resources/views/guest/components/wizard_3.blade.php
+++ resources/views/guest/components/wizard_3.blade.php
@@ -1,8 +1,54 @@
 <div class="wizard-content mt-2" id="wizard3">
     <div class="wizard-pane">
 
-        <div class="section-title">Detail Perhitungan</div>
-        <div class="table-responsive">
+        <div class="section-title">Hasil Diagnosa</div>
+        <div class="row">
+            <div class="col-4 text-center">
+                <div class="result">
+                    <div class="pricing">
+                        <div class="pricing-title">
+                            Developer
+                        </div>
+                        <div class="pricing-padding">
+                            <div class="pricing-price">
+                                <div>$19</div>
+                                <div>per month</div>
+                            </div>
+                            <div class="pricing-details">
+                                <div class="pricing-item">
+                                    <div class="pricing-item-icon"><i class="fas fa-check"></i></div>
+                                    <div class="pricing-item-label">1 user agent</div>
+                                </div>
+                                <div class="pricing-item">
+                                    <div class="pricing-item-icon"><i class="fas fa-check"></i></div>
+                                    <div class="pricing-item-label">Core features</div>
+                                </div>
+                                <div class="pricing-item">
+                                    <div class="pricing-item-icon"><i class="fas fa-check"></i></div>
+                                    <div class="pricing-item-label">1GB storage</div>
+                                </div>
+                                <div class="pricing-item">
+                                    <div class="pricing-item-icon"><i class="fas fa-check"></i></div>
+                                    <div class="pricing-item-label">2 Custom domain</div>
+                                </div>
+                                <div class="pricing-item">
+                                    <div class="pricing-item-icon bg-danger text-white"><i class="fas fa-times"></i></div>
+                                    <div class="pricing-item-label">Live Support</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pricing-cta">
+                            <a href="#">Subscribe <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="result">
+            <div class="col-12 col-md-4 col-lg-4">
+            </div>
+        </div>
+        {{-- <div class="table-responsive">
             <table class="table table-sm" id="processTable">
                 <thead>
                     <th scope="col">#</th>
@@ -12,7 +58,7 @@
                 </thead>
                 <tbody></tbody>
             </table>
-        </div>
+        </div> --}}
         
         <center>
             <button class="btn btn-icon icon-right btn-danger my-5" onclick="backToWizard2()">
